377a6e140ca48c8c8e62b533d3dc6baa02f2d36d
Purinut <reawjangpurinut@gmail.com>
2021-05-12 10:13:43 +0700

    add petition amount chart

--- routes/home.js
+++ routes/home.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
 
 const isLoggined = function(req, res, next) {
 	if(!req.session.user) {
@@ -8,9 +9,15 @@ const isLoggined = function(req, res, next) {
 	next();
 };
 
+const getPetition = require('../chart/get_petition_amount');
+
 router.get('/', isLoggined, function(req, res){
+	getPetition.getData();
+	let rawdata = fs.readFileSync('petition.json');
+	let petition = JSON.parse(rawdata);
+
 	if(req.session.depID == 'DE001' && req.session.posID == 'PO002'){
-		res.render('admin',{username: req.session.user});
+		res.render('admin',{username: req.session.user, petition: petition});
 	} 
 	else if(req.session.depID == 'DE001' && req.session.posID == 'PO003'){
 		res.render('assistant',{username: req.session.user});
