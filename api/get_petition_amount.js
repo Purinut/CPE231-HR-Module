1ea5b195b29e172814917af3bc9ee1b7abab9813
Purinut <reawjangpurinut@gmail.com>
2021-06-02 19:45:15 +0700

    fix petition chart

--- api/get_petition_amount.js
+++ api/get_petition_amount.js
@@ -4,11 +4,18 @@ const router = express.Router();
 const db = require('../dbp');
 
 router.get('/', async function(req, res) {
-	const query = `SELECT DATE_FORMAT(Petition_Date, '%m-%Y') AS DATE, COUNT(*) AS n
+	const query = `SELECT pd.DATE, IFNULL(dd.n, 0) AS n FROM
+					(SELECT DATE_FORMAT(CURRENT_DATE(), '%m-%Y') AS DATE
+					UNION SELECT DATE_FORMAT(DATE_SUB(CURRENT_DATE(), INTERVAL 1 MONTH), '%m-%Y') AS DATE
+					UNION SELECT DATE_FORMAT(DATE_SUB(CURRENT_DATE(), INTERVAL 2 MONTH), '%m-%Y') AS DATE
+					UNION SELECT DATE_FORMAT(DATE_SUB(CURRENT_DATE(), INTERVAL 3 MONTH), '%m-%Y') AS DATE
+					UNION SELECT DATE_FORMAT(DATE_SUB(CURRENT_DATE(), INTERVAL 4 MONTH), '%m-%Y') AS DATE) pd
+					LEFT JOIN
+					(SELECT DATE_FORMAT(Petition_Date, '%m-%Y') AS DATE, COUNT(*) AS n
 					FROM petition
 					GROUP BY YEAR(Petition_Date), MONTH(Petition_Date)
-					ORDER BY YEAR(Petition_Date), MONTH(Petition_Date) DESC
-					LIMIT 5;`;
+					ORDER BY YEAR(Petition_Date) DESC, MONTH(Petition_Date) DESC) dd
+					ON pd.DATE = dd.DATE`;
 	let result = await db(query);
 
 	labels = [];
@@ -23,6 +30,7 @@ router.get('/', async function(req, res) {
 		labels: labels.reverse(),
 		amount: amount.reverse()
 	}
+
 	res.send(data);
 })
 
